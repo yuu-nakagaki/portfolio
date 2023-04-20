11f89aa772cfe4c8574fb7a0e6966660b9d37dae
yuu-nakagaki <129018884+yuu-nakagaki@users.noreply.github.com>
2023-04-20 17:25:05 +0900

    4/20学校での作業終了

--- js/function.js
+++ js/function.js
@@ -2,20 +2,40 @@
 
 const hamBtn = document.querySelector('#btn');
 const gnav = document.querySelector('#gnav');
+const gnavNav = document.querySelector('#gnav__nav')
 
 const btnTop = document.querySelector('#btn__top');
 const btnMiddle = document.querySelector('#btn__middle');
 const btnBottom = document.querySelector('#btn__bottom');
 
-hamBtn.addEventListener('click', function(){
+// const headerHeight = 50;
+// let bodyHeight;
+// let scrollPos;
+
+hamBtn.addEventListener('click', function () {
     btnTop.classList.toggle('rotate-top');
     btnMiddle.classList.toggle('hide-middle');
     btnBottom.classList.toggle('rotate-bottom');
     gnav.classList.toggle('gnav-active');
+    gnavNav.classList.toggle('gnav__nav-active');
+    document.body.classList.toggle('menu-open');
+
+    // scrollPos = window.pageYOffset
+    // document.body.style.top = scrollPos * -1 + 'px';
+    // bodyHeight = window.innerHeight;
+    // gnav.style.height = bodyHeight - headerHeight + 'px';
 });
 
-gnav.addEventListener('click', function(){
+gnav.addEventListener('click', function () {
+    btnTop.classList.remove('rotate-top');
+    btnMiddle.classList.remove('hide-middle');
+    btnBottom.classList.remove('rotate-bottom');
     gnav.classList.remove('gnav-active');
+    gnavNav.classList.remove('gnav__nav-active');
+    document.body.classList.remove('menu-open');
+
+    // window.scrollTo(0, scrollPos)
+    // gnav.style.height = 0
 });
 
 
@@ -23,10 +43,8 @@ gnav.addEventListener('click', function(){
 
 const text = document.querySelector('.mvcover');
 
-//初期状態
-gsap.set(".mv__wrap", { autoAlpha: 1 });
+gsap.set(".mv__wrap", { autoAlpha: 1 }); //初期設定
 
-//スクロールをトリガーに、以下
 gsap.to(".mv__wrap", {
     autoAlpha: 0,
     textShadowBlur: 20,
