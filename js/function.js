75349abec245660900b4934918555231d47b023d
yuu-nakagaki <129018884+yuu-nakagaki@users.noreply.github.com>
2023-04-21 17:28:34 +0900

    4/21学校作業終了

--- js/function.js
+++ js/function.js
@@ -1,3 +1,22 @@
+
+//スクロールでヘッダータイトルが消える
+let beforePos = 0;
+let winScrollTop = 0;
+
+window.addEventListener('scroll', function(){
+    winScrollTop = this.scrollY;
+    if (winScrollTop >= beforePos){
+        if(winScrollTop >= 300) {
+            this.document.getElementById('header__ttlbox').classList.add('is-hide');
+        }
+    } else {
+        this.document.getElementById('header__ttlbox').classList.remove('is-hide');
+    }
+    beforePos = winScrollTop;
+});
+
+
+
 //ハンバーガーメニュー
 
 const hamBtn = document.querySelector('#btn');
@@ -8,9 +27,8 @@ const btnTop = document.querySelector('#btn__top');
 const btnMiddle = document.querySelector('#btn__middle');
 const btnBottom = document.querySelector('#btn__bottom');
 
-// const headerHeight = 50;
-// let bodyHeight;
-// let scrollPos;
+const scrollPos = window.pageYOffset;
+const bodyHeight = window.innerHeight;
 
 hamBtn.addEventListener('click', function () {
     btnTop.classList.toggle('rotate-top');
@@ -18,12 +36,9 @@ hamBtn.addEventListener('click', function () {
     btnBottom.classList.toggle('rotate-bottom');
     gnav.classList.toggle('gnav-active');
     gnavNav.classList.toggle('gnav__nav-active');
-    document.body.classList.toggle('menu-open');
+    // document.body.classList.toggle('menu-open'); //開く前の表示位置保持とセットなので後回し いったん解除
 
-    // scrollPos = window.pageYOffset
-    // document.body.style.top = scrollPos * -1 + 'px';
-    // bodyHeight = window.innerHeight;
-    // gnav.style.height = bodyHeight - headerHeight + 'px';
+
 });
 
 gnav.addEventListener('click', function () {
@@ -32,19 +47,15 @@ gnav.addEventListener('click', function () {
     btnBottom.classList.remove('rotate-bottom');
     gnav.classList.remove('gnav-active');
     gnavNav.classList.remove('gnav__nav-active');
-    document.body.classList.remove('menu-open');
+    // document.body.classList.remove('menu-open');
 
-    // window.scrollTo(0, scrollPos)
-    // gnav.style.height = 0
+    window.scrollTo(0, scrollPos);
 });
 
 
 //FVぼかしエフェクト
-
 const text = document.querySelector('.mvcover');
-
 gsap.set(".mv__wrap", { autoAlpha: 1 }); //初期設定
-
 gsap.to(".mv__wrap", {
     autoAlpha: 0,
     textShadowBlur: 20,
@@ -57,3 +68,47 @@ gsap.to(".mv__wrap", {
         markers: true
     }
 });
+
+
+//SKILL部フィルター
+const filterButtons = document.querySelectorAll('[data-filter]');
+const categoryContents = document.querySelectorAll('[data-category]');
+
+filterButtons.forEach((filterButton) => {
+    filterButton.addEventListener('click', buttonSwitch);
+    filterButton.addEventListener('click', categoryFilter);
+});
+
+function buttonSwitch() {
+    filterButtons.forEach((filterButton) => {
+        filterButton.classList.remove("is-active");
+        this.classList.add('is-active');
+    });
+}
+
+function categoryFilter() {
+    const buttonCategory = this.dataset.filter;
+    const targetContents = document.querySelectorAll('[data-category="' + buttonCategory + '"]');
+
+    categoryContents.forEach((categoryContent) => {
+        categoryContent.animate([{
+            opacity: 0
+        },
+        {
+            opacity: 1
+        }
+        ], {
+            duration: 600,
+        });
+
+        if (buttonCategory == 'all') {
+            categoryContent.classList.add('is-show');
+        } else {
+            categoryContent.classList.remove("is-show");
+            targetContents.forEach((targetContent) => {
+                targetContent.classList.add('is-show');
+            });
+        }
+
+    });
+}
